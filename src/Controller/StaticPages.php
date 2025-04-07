dab2f2477851adfc1da190758542d9c25d74dfd1
Noah Boos <nboosgamemaker@gmail.com>
2025-04-07 12:38:01 +0200

    Patch - Rebuilt previous codebase

---
+++ src/Controller/StaticPages.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StaticPages extends AbstractController {
+    #[Route('/', name: 'homepage')]
+    public function index(): Response {
+        return $this->render('base.html.twig', [
+            'title' => 'Page d\'accueil',
+        ]);
+    }
+}
